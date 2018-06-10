5c4a15141fe3be82c30f865f1be6fe55b312dc19
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-06-10 13:20:15 -0400

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// start the game
+
+// set variables - random target number, wins, losses, random value's for the crystals, current score
+
+// set random target number (19-120)
+
+// set random values for the crystal (1-12)
+
+// when clicking on specific crystal, add its value to the current score
+
+// when the current score equals the random target number, display "You Won" and reset the game with new crystal values
+
+// when the current score is more than the random target number, display "You loose" and reset the game with new crystal values
+
